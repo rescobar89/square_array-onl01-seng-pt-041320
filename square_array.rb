730441731437613d9ce88fe8c9d63300e0c8c489
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-21 16:58:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,11 +3,13 @@ def square_array(array)
   array.each do |number|
      rtn.push(number*number)
   end
+  #returning the value of rtn
   return rtn
 end
 
 lilo = [1, 2, 3]
 
+#returned value of a new array
 stitch = square_array(lilo)
 
 stitch.each do |alien|
