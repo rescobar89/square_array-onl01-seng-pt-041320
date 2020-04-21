d7fe72c6be1e15d2277b6050ac6ba67f75e8cb45
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-21 06:03:55 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  rtn = []
+  array.each do |number|
+     rtn.push(number*number)
+  end
+  return rtn
+end
+
+lilo = [1, 2, 3]
+
+stitch = square_array(lilo)
+
+stitch.each do |alien|
+  puts "#{alien}"
 end
